256a7ea26f82312283ea65cb7774d79c72ec47f9
PatrykKeska <PatrykKeska4@gmail.com>
2022-06-19 19:22:42 +0200

    connection for database added

--- utils/dbConnection.ts
+++ utils/dbConnection.ts
@@ -1,10 +1,12 @@
 import {createPool} from "mysql2/promise";
 
 export const pool = createPool({
-    host: 'localhost',
+    // host: 'localhost',
+    host:"0.0.0.0",
     user: 'root',
     password: 'root',
     database: 'company_assets',
     namedPlaceholders: true,
     decimalNumbers: true,
+    port:8889,
 })
