432c33d43ff222e306c7c65f977e3c1c001e3048
PatrykKeska <PatrykKeska4@gmail.com>
2022-06-19 19:25:10 +0200

    into index.js file added and implemented imports for router

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express, {json} from "express";
 import cors from 'cors';
 import {handleError} from "./utils/errors";
+import {addItemRouter} from "./routers/addItemRouter";
 
 
 const app = express();
@@ -9,5 +10,7 @@ app.use(cors({
     origin: "http://localhost:3000"
 }));
 app.use(json());
+app.use('/', addItemRouter);
+
 app.use(handleError)
-app.listen(3001, '0.0.0.0', () => console.log("Listening on http://localhost:3301"));
+app.listen(3001, '0.0.0.0', () => console.log("Listening on http://localhost:3001"));
