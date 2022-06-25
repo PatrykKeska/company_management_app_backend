3da6350af878d3af3d4795f8413a105ae3348990
PatrykKeska <PatrykKeska4@gmail.com>
2022-06-25 17:06:06 +0200

    new endpoints router imports added

--- index.ts
+++ index.ts
@@ -1,8 +1,10 @@
 import express, {json} from "express";
 import cors from 'cors';
 import {handleError} from "./utils/errors";
-import {addItemRouter} from "./routers/addItemRouter";
-import {storageRouter} from "./routers/storageRouter";
+import {addItemRouter} from "./routers/Products/addItemRouter";
+import {addPlaceRouter} from "./routers/Places/addPlaceRouter";
+import {storageRouter} from "./routers/Products/storageRouter";
+import {placesRouter} from "./routers/Places/placesRouter";
 
 
 const app = express();
@@ -13,9 +15,8 @@ app.use(cors({
 app.use(json());
 app.use('/', addItemRouter);
 app.use('/', storageRouter);
-// app.use(express.static('public'));
-
-
+app.use('/', addPlaceRouter);
+app.use('/', placesRouter);
 
 
 app.use(handleError)
