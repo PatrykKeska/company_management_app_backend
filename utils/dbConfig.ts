e95d794962727810a4d3d6a3edc80feb74c223f2
PatrykKeska <PatrykKeska4@gmail.com>
2022-07-11 19:56:16 +0200

    Db connection moved to file for feature data protection

---
+++ utils/dbConfig.ts
@@ -0,0 +1,11 @@
+
+export const dbConfig = {
+    host:"0.0.0.0",
+    user: 'root',
+    password: 'root',
+    database: 'company_assets',
+    namedPlaceholders: true,
+    decimalNumbers: true,
+    port:8889,
+
+}
